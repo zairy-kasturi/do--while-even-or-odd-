149ba5ac487d79d3789183f74499f32d08efd956
zairy-kasturi <55353904+zairy-kasturi@users.noreply.github.com>
2019-10-14 20:50:51 +0800

    Add files via upload
    do-while

---
+++ ConsoleApplication26.cpp
@@ -0,0 +1,38 @@
+
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	//declare 
+	int a;
+	char choose;
+
+	do
+	{
+		//requet input from user
+		cout << "Plz enter number :" << endl;
+		cin >> a;
+
+
+		//process: system will know what kind of number user press
+		if (a % 2 == 0)
+			cout << a << " Is even number " << endl;
+		else if (a % 2 != 0)
+			cout << a << " Is odd number " << endl;
+	
+
+		//ask user to wanna looping again or not ? 
+		cout << "do you want  looping again ?? :" << endl;
+		cout << "The answer should be Y/y or N/n only " << endl;
+		cin >> choose;
+		
+
+	} while (choose == 'y'|| choose == 'Y');
+
+	//greeting for user friendly
+	cout << "Thanks you " << endl;
+	cout << "Have a nice day " << endl;
+}
+
